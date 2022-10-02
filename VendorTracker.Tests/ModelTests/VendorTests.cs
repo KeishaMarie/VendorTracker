acdc50163a97ea89d54f2a9cc723836a7b2d3b50
Keisha Hepner <kmhepner90@gmail.com>
2022-10-01 20:37:37 -0700

    Updated Add Order test to pass with new Vendor Description Property

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -17,15 +17,16 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("test vendor");
+      Vendor newVendor = new Vendor("test vendor", "test description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
     [TestMethod]
     public void GetName_ReturnsName_String()
     {
-      string name = "Test Vendor";
-      Vendor newVendor = new Vendor(name);
+      string name = "test vendor";
+      string description = "test description";
+      Vendor newVendor = new Vendor(name, description);
       string result = newVendor.Name;
       Assert.AreEqual(name, result);
     }
@@ -33,8 +34,9 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void GetId_ReturnsVendorId_Int()
     {
-      string name = "Test Vendor";
-      Vendor newVendor = new Vendor(name);
+      string name = "test vendor";
+      string description = "test description";
+      Vendor newVendor = new Vendor(name, description);
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
@@ -43,9 +45,11 @@ namespace VendorTracker.Tests
     public void GetAll_ReturnsAllVendorObjects_VendorList()
     {
       string name01 = "Suzie's Cafe";
+      string description01 = "Cafe and Bakery";
       string name02 = "Cat's Crazy Cupcakes";
-      Vendor newVendor1 = new Vendor(name01);
-      Vendor newVendor2 = new Vendor(name02);
+      string description02 = " Sweet Shop";
+      Vendor newVendor1 = new Vendor(name01, description01);
+      Vendor newVendor2 = new Vendor(name02, description02);
       List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
       List<Vendor> result = Vendor.GetAll();
       CollectionAssert.AreEqual(newList, result);
@@ -55,9 +59,11 @@ namespace VendorTracker.Tests
     public void Find_ReturnsCorrectVendor_Vendor()
     {
       string name01 = "Suzie's Cafe";
+      string description01 = "Cafe and Bakery";
       string name02 = "Cat's Crazy Cupcakes";
-      Vendor newVendor1 = new Vendor(name01);
-      Vendor newVendor2 = new Vendor(name02);
+      string description02 = "Sweet Shop";
+      Vendor newVendor1 = new Vendor(name01, description01);
+      Vendor newVendor2 = new Vendor(name02, description02);
       Vendor result = Vendor.Find(2);
       Assert.AreEqual(newVendor2, result);
     }
@@ -69,10 +75,20 @@ namespace VendorTracker.Tests
       Order newOrder = new Order(description);
       List<Order> newList = new List<Order> { newOrder };
       string name = "Suzie's Cafe";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, description);
       newVendor.AddOrder(newOrder);
       List<Order> result = newVendor.Orders;
       CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string name = "test vendor";
+      string description = "test description";
+      Vendor newVendor = new Vendor(name, description);
+      string result = newVendor.Description;
+      Assert.AreEqual(description, result);
     }
   }
 }
